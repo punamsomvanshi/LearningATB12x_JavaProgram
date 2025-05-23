438dfc8f86070a45986a4c32691ed9da10e04e9d
punamsomvanshi <punamsomvanshi29@gmail.com>
2025-05-23 13:14:22 +0530

    Task

---
+++ src/main/java/Tasks/Task_01.java
@@ -0,0 +1,20 @@
+package Tasks;
+
+public class Task_01 {
+    public static void main(String[] agrs){
+        int num = 5;
+        System.out.println("Table of 5 :");
+
+        System.out.printf("%dx1=%d",num,num*1).println();
+        System.out.printf("%dx2=%d",num,num*2).println();
+        System.out.printf("%dx3=%d",num,num*3).println();
+        System.out.printf("%dx4=%d",num,num*4).println();
+        System.out.printf("%dx5=%d",num,num*5).println();
+        System.out.printf("%dx6=%d",num,num*6).println();
+        System.out.printf("%dx7=%d",num,num*7).println();
+        System.out.printf("%dx8=%d",num,num*8).println();
+        System.out.printf("%dx9=%d",num,num*9).println();
+        System.out.printf("%dx10=%d",num,num*10).println();
+
+    }
+}
