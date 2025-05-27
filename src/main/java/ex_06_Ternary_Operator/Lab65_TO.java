7ffc9da3bb283dd88f4315477577fbcbe89ac93c
punamsomvanshi <punamsomvanshi29@gmail.com>
2025-05-27 19:25:50 +0530

    17/05/2025 Part-2

---
+++ src/main/java/ex_06_Ternary_Operator/Lab65_TO.java
@@ -0,0 +1,10 @@
+package ex_06_Ternary_Operator;
+
+public class Lab65_TO {
+    public static void main(String[] args) {
+        int number = -5;
+        String result = number > 0 ? "positive" : "negative";
+//        int result = number > 0 ? "positive" : "negative";
+        System.out.println(result);
+    }
+}
---
+++ src/main/java/ex_06_Ternary_Operator/Lab67_TO_INTERVIEW_Max_Two_TO.java
@@ -0,0 +1,10 @@
+package ex_06_Ternary_Operator;
+
+public class Lab67_TO_INTERVIEW_Max_Two_TO {
+    public static void main(String[] args) {
+        int a=30;
+        int b=20;
+        int result= a>b ? a :b;
+        System.out.println("GreaterNumber =" + result);
+    }
+}
